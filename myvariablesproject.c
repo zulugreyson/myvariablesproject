b284631d4809e0f89003d45a7560ccb6d339a8d9
GREYSON ZULU <gzudgreyson@gmail.com>
2025-12-26 02:16:00 +0200

    Add files via upload
    This program:
    1. Declares variables for name, age, and GPA
    2. Stores values in these variables
    3. Displays the output in the specified format

---
+++ myvariablesproject.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Greyson";
+    int age = 30;
+    float gpa = 3.8;
+
+    printf("name: %s\n", name);
+    printf("age: %d\n", age);
+    printf("GPA: %.1f\n", gpa);
+
+    return 0;
+
+ }
